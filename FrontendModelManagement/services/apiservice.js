d383d4a132a4fbfae967dfba87be6bb1a2f2f92c
lauestlauest <76699249+lauestlauest@users.noreply.github.com>
2023-05-02 10:42:37 +0200

    dd

--- FrontendModelManagement/services/apiservice.js
+++ FrontendModelManagement/services/apiservice.js
@@ -1,7 +1,59 @@
-import React from "react";
+let api_base_url = 'https://localhost:44368/api';
 
-const ApiService = () => {
-  return <div>ApiService</div>;
-};
+export async function getData(endpoint,) {
+  const config = {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem("token"),
+      'Content-Type': 'application/json'
+    }
+  }
 
-export default ApiService;
+  return doFetch(endpoint, config);
+}
+
+export async function postData(endpoint, body,) {
+  const config = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem("token"),
+      'Content-Type': 'application/json',
+    },
+  }
+  return doFetch(endpoint, config);
+}
+
+export async function updateData(endpoint, body) {
+  const config = {
+    method: 'PUT',
+    body: JSON.stringify(body),
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem("token"),
+      'Content-Type': 'application/json',
+    },
+  }
+  return doFetch(endpoint, config);
+}
+
+export async function deleteData(endpoint) {
+  const config = {
+    method: 'DELETE',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem("token"),
+      'Content-Type': 'application/json',
+    },
+  }
+
+  return doFetch(endpoint, config);
+}
+
+async function doFetch(endpoint, config) {
+  let response = await window.fetch(`${api_base_url}/${endpoint}`, config);
+  if (response.ok) {
+    return await response.json();
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
