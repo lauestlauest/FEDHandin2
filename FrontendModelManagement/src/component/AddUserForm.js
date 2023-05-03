a4c2806e7e6a4e745e785d6199ccfec7dc792469
lauestlauest <76699249+lauestlauest@users.noreply.github.com>
2023-05-03 11:30:31 +0200

    working for models

--- FrontendModelManagement/src/component/AddUserForm.js
+++ FrontendModelManagement/src/component/AddUserForm.js
@@ -7,16 +7,27 @@ import {AddUserService} from "../services/adduserservice"
 
 export function AddUserForm() {
 
-   const {register, handleSubmit} = useForm({ shouldUseNativeValidation: true });
+   const {register, handleSubmit, unregister} = useForm({ shouldUseNativeValidation: true });
+   
    const [job , setjob] = useState(1);
    
 
   const onSubmit = async (data) => {
+    console.log(JSON.stringify(data))
     AddUserService( data, job)
   }
 
   useEffect(() => {
     document.title = `job number is ${job}`; //for testing
+    if(job== 1){
+    unregister(['phoneNo', 'addresLine1'
+          , 'addresLine2', 'zip' 
+          , 'city' , 'country' 
+          , 'birthdate' , 'nationality'
+          , 'height', 'shoeSize'
+          ,  'haircolor', 'eyeColor'
+          , 'comments' ])
+    }
   },[job]);
  
     
@@ -24,6 +35,12 @@ export function AddUserForm() {
   return (
     <div className='view'>
         <form onSubmit= {handleSubmit(onSubmit)}>  
+
+        <select onChange={(event) => setjob(event.target.value)}>
+            <option value='1' >Manager</option>
+            <option value='0'>Model</option>
+        </select>
+         <br/>
           
         <input {...register("firstName")} placeholder="First name"/>
           <br/>
@@ -37,11 +54,7 @@ export function AddUserForm() {
         <input {...register("password", { required: true })} type="Password" placeholder="Password"/>
           <br/>
 
-        <select onChange={(event) => setjob(event.target.value)}>
-            <option value='1' >Manager</option>
-            <option value='0'>Model</option>
-        </select>
-         <br/>
+       
 
           <input type="submit"/>
         </form>
@@ -51,7 +64,69 @@ export function AddUserForm() {
   else{
     return(
 
-      <h1>fff</h1>
+      <div className='view'>
+        <form onSubmit= {handleSubmit(onSubmit)}>  
+
+        <select onChange={(event) => setjob(event.target.value)} >
+            <option value='1' >Manager</option>
+            <option value='0'>Model</option>
+        </select>
+         <br/>
+          
+        <input {...register("firstName")} placeholder="First name"/>
+        <br/>
+  
+        <input {...register("lastName", { required: true })} placeholder="Last name" />
+        <br/>
+
+        <input {...register("email", { required: true })} type="email" placeholder="Email"/>
+        <br/>
+
+        <input {...register("phoneNo", { required: true })} type="number" placeholder="PhoneNo"/>
+        <br/>
+
+        <input {...register("addresLine1", { required: true })} placeholder="AddresLine1"/>
+        <br/>
+
+        <input {...register("addresLine2", { required: true } )} placeholder="AddresLine2"/>
+        <br/>
+
+        <input {...register("zip", { required: true })} type="number" placeholder="Zip"/>
+        <br/>
+
+        <input {...register("city", { required: true })} placeholder="City"/>
+        <br/>
+
+        <input {...register("country", { required: true })} placeholder="Country"/>
+        <br/>
+
+        <input {...register("birthdate", { required: true })} type="datetime-local" placeholder="Brithdate"/>
+        <br/>
+
+        <input {...register("nationality", { required: true })} placeholder="Nationality"/>
+        <br/>
+
+        <input {...register("height", { required: true } )} type="number" placeholder="Height"/>
+        <br/>
+
+        <input {...register("shoeSize", { required: true } )} type="number" placeholder="ShoeSize"/>
+        <br/>
+
+        <input {...register("haircolor", { required: true } )}  placeholder="HairColor"/>
+        <br/>
+
+        <input {...register("eyeColor", { required: true } )}  placeholder="EyeColor"/>
+        <br/>
+
+        <input {...register("comments", { required: true } )}  placeholder="Comments"/>
+        <br/>
+
+        <input {...register("password", { required: true })}  placeholder="Password"/>
+        <br/>
+
+          <input type="submit"/>
+        </form>
+    </div>
 
     );
   }
